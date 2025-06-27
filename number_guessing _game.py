4bdb209cb6c0008219bdb6d8bea3e9f3bd8c3c80
Harshit-git1 <mittalharshit1111@gmail.com>
2025-06-27 10:15:25 +0530

    Update number_guessing _game.py
    Improved number guessing game logic and grammar

--- number_guessing _game.py
+++ number_guessing _game.py
@@ -1,18 +1,20 @@
 import random
 
 secret_number = random.randint(1, 100)
+guess = 0
 
 # game logic
 while True:
     try:
         # input
         player = int(input("Enter your guess from 1 to 100 here:"))
+        guess +=1
         if player > secret_number:
             print("Too High!, Try Again.")
         elif player < secret_number:
             print("Too Low!, Try Again.")
         else:
-            print("Please enter a number between 1 to 100.")
+            print("Congrats!, you guesses the number in {guess} attempt(s)")
     except ValueError:
         print("Invalid Input. Please enter a number from 1 to 100.")
 
